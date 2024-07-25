dc560e5f4ac19bc1dc56efc5a6e0dd80131a3597
Somika <rasqq2qqq@gmail.com>
2024-07-25 16:28:55 +0300

    доработать покрытие тестами; Изменить логику методов getEpic, getSubtask

---
+++ tests/InMemoryHistoryManagerTests.java
@@ -0,0 +1,30 @@
+import controllers.InMemoryHistoryManager;
+import model.Task;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InMemoryHistoryManagerTests {
+
+    //добавляемые в HistoryManager, удаляют предыдущую версию задачи и её данных.
+    @Test
+    public void testSavingPreviousVersionOfTask() {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+
+        Task originalTask = new Task("Оригинальная задача", "Описание задачи");
+        originalTask.setId(1); // Установка ID для теста
+
+        historyManager.add(originalTask);
+
+        originalTask.setName("Измененное имя");
+        originalTask.setDescription("Измененное описание");
+
+        historyManager.add(originalTask);
+
+        ArrayList<Task> history = (ArrayList<Task>) historyManager.getHistory();
+
+        assertEquals(1, history.size(), "В истории должна быть одна версия задачи.");
+    }
+}
---
+++ tests/TaskTests.java
@@ -0,0 +1,30 @@
+import model.Epic;
+import model.Subtask;
+import model.Task;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TaskTests {
+    //Экземпляры класса Task равны друг другу, если равен их id
+    @Test
+    void taskEqualsbySameId() {
+        Task task1 = new Task("Test 1", "Описание 1");
+        Task task2 = new Task("Test 2", "Описание 2");
+        task1.setId(1);
+        task2.setId(1);
+
+        assertEquals(task1.getId(), task2.getId(), "Задачи должны быть равны по ID");
+    }
+
+    //Наследники класса Task равны друг другу, если равен их id
+    @Test
+    void epicEqualsSubtaskBySameId() {
+        Epic epic = new Epic("Epic Test", "Epic Description");
+        Subtask subtask = new Subtask("Subtask Test", "Subtask Description", 1);
+        epic.setId(1);
+        subtask.setId(1);
+
+        assertEquals(epic.getId(), subtask.getId(), "Наследники должны быть равны по ID");
+    }
+}
